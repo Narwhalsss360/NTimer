a4e9f7aa4ec3374bbbddf89537ba3147b31c3a34
Franklyn Vasquez <franklynvasquez24@gmail.com>
2022-05-15 22:55:25 -0400

    [CHANGE] literals defenition

--- src/TimeLiterals.h
+++ src/TimeLiterals.h
@@ -1,8 +1,8 @@
 #ifndef TimeLiterals_h
 #define TimeLiterals_h
 
-unsigned long operator"" _hz(unsigned long long);
-unsigned long operator"" _s(unsigned long long);
-unsigned long operator"" _m(unsigned long long);
+unsigned long operator"" hz(unsigned long long);
+unsigned long operator"" s(unsigned long long);
+unsigned long operator"" m(unsigned long long);
 
 #endif
