b0c81280b5094d7fa1894caad64b2f0a1e948e48
Franklyn Vasquez <franklynvasquez24@gmail.com>
2022-05-15 22:56:24 -0400

    [FIX] Syntax rename

--- src/TimeLiterals.cpp
+++ src/TimeLiterals.cpp
@@ -1,16 +1,16 @@
 #include "TimeLiterals.h"
 
-unsigned long operator"" _hz(unsigned long long hertz)
+unsigned long operator"" hz(unsigned long long hertz)
 {
     return hertz / 1000;
 }
 
-unsigned long operator"" _s(unsigned long long seconds)
+unsigned long operator"" s(unsigned long long seconds)
 {
     return seconds * 1000;
 }
 
-unsigned long operator"" _m(unsigned long long minutes)
+unsigned long operator"" m(unsigned long long minutes)
 {
     return minutes * 60000;
 }
